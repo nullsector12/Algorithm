f33701d5ee708f6b18284c2eda8c57adb176ed90
KimSeungYeon <rlamd0412@gmail.com>
2020-07-02 18:06:04 +0900

    algorithm

---
+++ Algorithm-Ex/src/algorithm/Bubble_sort.java
@@ -0,0 +1,61 @@
+package algorithm;
+
+import java.util.ArrayList;
+
+public class Bubble_sort {
+	
+	/*
+	 * 2020. 07. 02 작성
+	 * 알고리즘 문제 연습
+	 * Title : Bubble sort
+	 * Goal	:	1. Bubble sort 에 대해 설명 
+	 * 			2. Bubble sort 과정에 대해 설명
+	 * 			3. Bubble sort 구현
+	 * 
+	 * 조건 : 서로 인접한 두 원소의 대소를 비교 -> 조건에 맞지 않는다면 자리를 교환하면서 정렬
+	 * */
+	
+	public static void main(String[] args) {
+		
+		// ArrayList를 생성
+		ArrayList <Integer> list = new ArrayList<Integer>();
+		
+		// ArrayList 배열에 random 값을 넣어줌
+		for (int i = 0; i < 10; i++) {
+			int randNum = (int)(Math.random()*100)+1;
+			list.add(randNum);
+		}
+		
+		// 저장된 list 배열값 출력
+		System.out.println(list);
+		
+		// list의 size만큼 아래 for문을 반복
+		for (int j = 0; j < list.size(); j++) {
+			
+			// 인덱스값을 반환하여 list.size-1번만큼 for문 반복
+			// size-1 하는 이유 : i값을 인덱스값으로 사용하기 때문에 
+			// 반복을 통해 증가하는 i값(list.size() = 10 / 인덱스 범위는 0~9)이 인덱스 범위를 넘어가지 않기위해 -1을 해준다.
+			for(int i = 0;i < list.size()-1; i++) {
+			
+				// 비교할 숫자를 index i번에서 출력
+				int checkNum0 = list.get(i);
+				
+				// 비교해줄 숫자를 index i+1번에서 출력
+				int checkNum1 = list.get(i+1);
+				
+				// 조건식을 통해 checkNum0 과 checkNum1 을 비교 후 위치를 정렬해줌
+				// checkNum0 이 checkNum1 보다 큰 경우
+				 if(checkNum0 > checkNum1) {
+					// checkNum1 값이 소멸하지 않도록 변수에 저장해줌
+					int savecheckNum1 = checkNum1;
+					
+					// set을 통해 i+1 번에 checkNum0 값을 set 해줌
+					list.set(i+1, checkNum0);
+					// i번지에 변수로 저장해둔 checkNum1 값을 넣어줌
+					list.set(i, savecheckNum1);	
+				}
+			}
+		}
+		System.out.println(list);
+	}
+}
---
+++ Algorithm-Ex/src/algorithm/Selection_sort.java
@@ -0,0 +1,65 @@
+package algorithm;
+
+import java.util.ArrayList;
+
+public class Selection_sort {
+	
+	/*
+	 * 2020. 07. 02 작성
+	 * 알고리즘 문제 연습
+	 * Title : Selection sort
+	 * Goal	:	1. Selection sort 에 대해 설명 
+	 * 			2. Selection sort 과정에 대해 설명
+	 * 			3. Selection sort 구현
+	 * 
+	 * 조건 : 주어진 배열에서 최소값 찾기 -> 해당 최소값을 맨 앞에 위치한 값과 교체-> 맨 처음 위치를 뺀 나머지 위치에서 반복
+	 * */
+	
+	public static void main(String[] args) {
+		
+
+		ArrayList <Integer> list = new ArrayList<Integer>();
+		
+
+		for (int i = 0; i < 10; i++) {
+			int randNum = (int)(Math.random()*100)+1;
+			list.add(randNum);
+		}
+
+		System.out.println(list);
+		
+		// 최소값 Index번호와 변경할 값을 저장할 변수 선언
+		int minIndex, change;
+		// list 내부의 최소값 비교 반복을 위한 for문
+		// list.size()-1 하는 이유 : 마지막 반복이 size인 10의 -1번째에서 끝나도록 하기 위해
+		// size = 10일 때 인덱스 번호는 0 ~ 9
+		for (int i = 0; i < list.size()-1; i++) {
+			
+			// list 내의 최소값을 저장할 Index 설정
+			minIndex = i;
+			
+			// list 내의 값들과 비교하기 위한 for 반복문
+			for(int j = i+1; j < list.size(); j++) {
+				// i+1번 인덱스의 값이 i번 인덱스 값보다 작으면
+				 if(list.get(j) < list.get(minIndex)) {   
+					 // 최소값 Index 번호는 j로 변경
+					 minIndex = j;
+					 
+					// list[1] < list [0] =  minIndex -> [1]
+				}
+			}
+			// 자리 변경을 위해 change 에  minIndex의 값 저장 
+			// change = list[1]
+			change = list.get(minIndex);
+			
+			// list의 minIndex=j번 인덱스에 i번 인덱스의 값 저장
+			// list[1] = list[0]
+			list.set(minIndex, list.get(i)) ;
+		
+			// list의 i번 인덱스에 minIndex=j번 인덱스의 값 저장
+			// list[0] = list[1]
+			list.set(i, change);
+		}
+		System.out.println(list);
+	}
+}
