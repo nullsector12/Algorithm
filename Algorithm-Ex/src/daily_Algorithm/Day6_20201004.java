af876f7f2fce97328943eb353da857ab95e9a5ea
nullsector12 <rlatmd017@naver.com>
2020-10-05 00:09:05 +0900

    20201004 Daily Algorithm Day6

--- Algorithm-Ex/src/daily_Algorithm/Day6_20201004.java
+++ Algorithm-Ex/src/daily_Algorithm/Day6_20201004.java
@@ -3,6 +3,14 @@ package daily_Algorithm;
 import java.io.*;
 import java.util.*;
 
+/*
+ *  2020. 10. 04 Daily Algorithm 
+ *  Day3 - baekjoon 단계별 문제풀이 문제번호 10871번 
+ * 	x보다 작은 수 
+ * 
+ * 
+ */
+
 public class Day6_20201004 {
 
 	public static void main(String[] args) throws IOException{
@@ -13,7 +21,18 @@ public class Day6_20201004 {
 		int N = Integer.parseInt(st.nextToken());
 		int X = Integer.parseInt(st.nextToken());
 		
-		for()
+		int A[] = new int[N];
+		StringTokenizer num = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			A[i] = Integer.parseInt(num.nextToken());
+			
+			if(A[i]< X) {
+				bw.write(A[i]+" ");
+			}
+			
+		}
+		bw.flush();
+		
 	}
 
 }
