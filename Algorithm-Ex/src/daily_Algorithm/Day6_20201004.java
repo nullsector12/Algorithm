0dcefd13e5d1face19c541eebbc7a34bbc255329
nullsector12 <rlatmd017@naver.com>
2020-10-04 23:52:12 +0900

    20201004 Daily Algorithm Day6

---
+++ Algorithm-Ex/src/daily_Algorithm/Day6_20201004.java
@@ -0,0 +1,19 @@
+package daily_Algorithm;
+
+import java.io.*;
+import java.util.*;
+
+public class Day6_20201004 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		
+		for()
+	}
+
+}
