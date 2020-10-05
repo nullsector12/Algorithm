b930d73e6e94eea7e517293c68fb757a94459a6c
nullsector12 <rlatmd017@naver.com>
2020-10-06 04:09:40 +0900

    20201005 Daily Algorithm Day7

--- Algorithm-Ex/src/daily_Algorithm/Day7_20201005.java
+++ Algorithm-Ex/src/daily_Algorithm/Day7_20201005.java
@@ -2,12 +2,55 @@ package daily_Algorithm;
 
 /*
  *  2020. 10. 05 Daily Algorithm 
- *  Day7 - baekjoon 단계별 문제풀이 문제번호 10871번 
- * 	x보다 작은 수 
+ *  Day7 - baekjoon 단계별 문제풀이 문제번호 10952번, 10951번, 1110번
+ * 	A+B - 5 / A+B - 4 / 더하기 사이클
+ * 
+ * 	EOF?
+ * 	End Of File (파일의 끝)
  * 
  * 
  */
+import java.io.*;
+import java.util.*;
 
 public class Day7_20201005 {
-
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		// A+B - 5
+//		while(true) {
+//			st = new StringTokenizer(br.readLine());
+//			int A = Integer.parseInt(st.nextToken());
+//			int B = Integer.parseInt(st.nextToken());
+//			int sum = A+B;
+//			
+//			
+//			if(sum != 0) {
+//				bw.write(sum+"\n");
+//				
+//				
+//			} else if(sum == 0){
+//				bw.flush();
+//				return;
+//			}
+//		} // while & A+B - 5 end
+		
+		// A+B - 4 (EOF)
+//		String eof;
+//		
+//		while((eof=br.readLine())!=null) {
+//			
+//			st = new StringTokenizer(eof);
+//			int A = Integer.parseInt(st.nextToken());
+//			int B = Integer.parseInt(st.nextToken());
+//			int sum = A+B;
+//			bw.write(sum+"\n");
+//		} while end
+//		bw.flush(); // A+B - 4 end
+		
+		
+	}
 }
