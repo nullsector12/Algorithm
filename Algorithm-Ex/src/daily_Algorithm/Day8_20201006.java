62c89f185f3ec91f36e23cea1c0a62dc58e7f3b7
nullsector12 <rlatmd017@naver.com>
2020-10-08 00:00:49 +0900

    20201006 Daily Algorithm Day8

--- Algorithm-Ex/src/daily_Algorithm/Day8_20201006.java
+++ Algorithm-Ex/src/daily_Algorithm/Day8_20201006.java
@@ -20,20 +20,51 @@ public class Day8_20201006 {
 		StringTokenizer st;
 		
 		int N = Integer.parseInt(br.readLine());
-		int arr[]= new int[N];
-		int max = 0;
-		int min = 0;
+		int[] arr = new int[N]; 
+		
 		
 		st = new StringTokenizer(br.readLine());
+//		for(int i=0; i<N; i++) {
+//			int num = Integer.parseInt(st.nextToken());
+//			arr[i] = num;
+//		}
+//		 
+//		Arrays.sort(arr);
+//		bw.write(arr[0]+" "+arr[N-1]);
+//		bw.flush();
+
+		int max = -1000000;
+		int min = 1000000;
+		
+//		for(int i=0; i<N; i++) {
+//			int num = Integer.parseInt(st.nextToken());
+//			if(num > max) {
+//				max = num;
+//			}
+//			if (num < min) {
+//				min = num;
+//			}
+//		}
+//		bw.write(min+" "+max);
+//		bw.flush();
+		
 		for(int i=0; i<N; i++) {
 			int num = Integer.parseInt(st.nextToken());
 			arr[i] = num;
 		}
 		
-		Arrays.sort(arr);
-		bw.write(arr[0]+" "+arr[N-1]);
-		bw.flush();
-
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<arr.length; j++) {
+				int chk = arr[j];
+				if(arr[i]<chk) {
+					arr[j] = arr[i];
+					arr[i] = chk;
+				}
+			}
+		}
+			bw.write(arr[0]+" "+arr[N-1]);
+			bw.flush();
+			System.out.println(Arrays.toString(arr));
+		}
 	}
 
-}
