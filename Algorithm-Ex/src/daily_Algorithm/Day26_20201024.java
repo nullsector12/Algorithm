7cb8082e64c333ba38c82679712456b0a18532ea
nullsector12 <63329386+nullsector12@users.noreply.github.com>
2020-10-25 16:13:39 +0900

    20201024 Daily Algorithm Day26

---
+++ Algorithm-Ex/src/daily_Algorithm/Day26_20201024.java
@@ -0,0 +1,25 @@
+package daily_Algorithm;
+
+/*
+ *  2020. 10. 24 Daily Algorithm 
+ *  Day26 - baekjoon 단계별 문제풀이 문제번호 1152
+ * 	단어의 개수
+ * 
+ */
+
+import java.io.*;
+import java.util.*;
+
+
+public class Day26_20201024 {
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		System.out.println(st.countTokens());
+	}
+}
