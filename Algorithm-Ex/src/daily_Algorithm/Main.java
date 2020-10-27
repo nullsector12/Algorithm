010a5d64042697dbff17ba2a3d2143ad7ef13a5c
nullsector12 <rlatmd017@naver.com>
2020-10-28 00:21:44 +0900

    short Cording ref

--- Algorithm-Ex/src/daily_Algorithm/Main.java
+++ Algorithm-Ex/src/daily_Algorithm/Main.java
@@ -1,9 +1,29 @@
 package daily_Algorithm;
 
-interface Main{static void main(String[]a)throws Exception{
-	int n,s=0;
-	while((n=System.in.read()-59)>-1) {
-		s+=(n-n/24-n/31)/3+1;
+import java.util.*;
+
+class Main{
+	public static void main(String[]t){
+		Scanner k=new Scanner(System.in);
+		int n=k.nextInt(),i,s=0,b,d=97;
+		char[]c,a;
+		for(;n>0;n--){
+			c=k.next().toCharArray();
+			a=new char[26];
+			b=0;
+			a[c[0]-d]=1;
+			for(i=1;i<c.length;i++){
+				if(c[i-1]!=c[i]) {
+					if(a[c[i]-d]>0){
+					b=1;
+					break;
+					}else { 
+						a[c[i]-d]=1;
+					}
+				}
+			}
+			s+=b>0?0:1;
+			}
+		System.out.print(s);
+		}
 	}
-	System.out.print(s);}
-}
