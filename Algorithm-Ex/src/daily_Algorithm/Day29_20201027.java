d85df7e12c04356621827fc89cb3b2b1a1f1bd00
nullsector12 <63329386+nullsector12@users.noreply.github.com>
2020-10-27 16:51:24 +0900

    20201027 Daily Algorithm Day29

---
+++ Algorithm-Ex/src/daily_Algorithm/Day29_20201027.java
@@ -0,0 +1,20 @@
+package daily_Algorithm;
+
+/*
+ *  2020. 10. 26 Daily Algorithm 
+ *  Day28 - baekjoon 단계별 문제풀이 문제번호 2941
+ * 	크로아티아 알파벳
+ * 
+ */
+
+import java.io.*;
+
+public class Day29_20201027 {
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+	}
+}
