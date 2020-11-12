9d445b711979219f00f7a5dd0ce58f2e38156ac5
nullsector12 <rlamd0412@gmail.com>
2020-11-12 18:32:55 +0900

    20201110 Daily Algorithm Day37

--- Algorithm-Ex/src/daily_Algorithm/Day37_20201110.java
+++ Algorithm-Ex/src/daily_Algorithm/Day37_20201110.java
@@ -11,14 +11,16 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Day37_20201110 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+	static StringTokenizer st;
+	static boolean[][] ladder;
 	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+	public static boolean[][] makeLadder(boolean[][] ladder) throws IOException {
 		
-		boolean[][] stick = new boolean[12][7];
-		
-		System.out.println(stick[0].length);
+		System.out.println("만드실 사다리 정보를 입력해주세요.");
+		System.out.println("사다리 만드는 법 : 1~10의 범위의 숫자 + 공백 + 1~"+ladder[0].length+"의 범위의 숫자를 입력하고"
+		+"\n"+"enter를 누르시면 다음 사다리를 입력 할 수 있고,"
+		+"\n"+"사다리 입력이 완료됐다면 x를 입력하고 enter하시면 됩니다.");
 		
 		String makeLadder = "";
 		while(makeLadder != "x") {
@@ -33,23 +35,45 @@ public class Day37_20201110 {
 			}else {
 				int columm = Integer.parseInt(check);
 				int row = Integer.parseInt(st.nextToken());
-				stick[columm][row] = true;
+				ladder[columm][row] = true;
 			}
 		}
+		return ladder;
+	}
+	
+	public static void game(boolean[][] ladder) throws IOException {
 		
-		System.out.println("시작점을 입력하세요");
-		int startPoint = Integer.parseInt(br.readLine()); 
-		for(int i=1; i<stick.length; i++) {
-			System.out.println("현재 포인트는 ("+i+", "+startPoint+")");
-			if(startPoint > 1 && stick[i][startPoint-1]) {
-				startPoint -= 1;
-				System.out.println("현재 포인트는 ("+i+", "+startPoint+")");
-				
-			}else if(stick[i][startPoint]) {
-				startPoint += 1;
-				System.out.println("현재 포인트는 ("+i+", "+startPoint+")");
+		while(true) {
+			System.out.println("시작점을 입력하세요");
+			String startPoint = br.readLine();
+			if(startPoint.equals("x")) {
+				System.out.println("게임을 종료합니다!");
+				break;
 			}
+			int point = Integer.parseInt(startPoint);
+			for(int i=1; i<ladder.length; i++) {
+				System.out.println("현재 포인트는 ("+i+", "+point+")");
+				if(point > 1 && ladder[i][point-1]) {
+					point -= 1;
+					System.out.println("현재 포인트는 ("+i+", "+point+")");
+					
+				}else if(ladder[i][point]) {
+					point += 1;
+					System.out.println("현재 포인트는 ("+i+", "+point+")");
+				}
+			}
+			System.out.println("도착! "+point);
 		}
-		System.out.println("도착!"+startPoint);
+	}
+	
+	public static void main(String[] args) throws IOException{
+		
+		System.out.print("게임에 참여할 인원이 몇명인가요? : ");
+		
+		ladder = new boolean[12][Integer.parseInt(br.readLine())+1];
+		
+		game(makeLadder(ladder));
+		
+
 	}
 }
