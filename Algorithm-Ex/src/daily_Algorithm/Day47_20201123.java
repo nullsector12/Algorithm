97cdc6b8d1f0f8575cd07b1cdf121ba11ad6b89c
nullsector12 <rlamd0412@gmail.com>
2020-11-23 13:51:04 +0900

    20201123 Daily Algorithm Day47

--- Algorithm-Ex/src/daily_Algorithm/Day47_20201123.java
+++ Algorithm-Ex/src/daily_Algorithm/Day47_20201123.java
@@ -19,13 +19,17 @@ public class Day47_20201123 {
 		// a=b일 경우
 		if(a == b) {
 			return a;
+		// a가 b보다 큰 경우 
 		}else if(a>b) {
+			// answer를 a로 변경
 			answer = a;
+			// for문을 통해 b+1씩 
 			for(; b<a; b++) {
 				answer = answer+b;
 			}
-			
+		// b가 a보다 큰 경우	
 		}else {
+			// a가 큰 경우와 동일(a와 b 자리만 변경)
 			answer = b;
 			for(; a<b; a++) {
 				answer = answer+a;
