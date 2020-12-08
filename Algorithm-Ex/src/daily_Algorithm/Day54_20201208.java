dd9323d22c0c2ebe5cc076ce60d50515dff84850
nullsector12 <rlamd0412@gmail.com>
2020-12-08 20:55:44 +0900

    20201208 Daily Algorithm Day54

---
+++ Algorithm-Ex/src/daily_Algorithm/Day54_20201208.java
@@ -0,0 +1,36 @@
+package daily_Algorithm;
+
+
+import java.util.*;
+
+/*
+ *  2020. 12. 01 Daily Algorithm 
+ *  Day52 - Programmers Cording Test Level 1
+ *  서울에서 김서방 찾기
+ * 
+ */
+
+public class Day54_20201208 {
+	
+	public static String solution(String[] seoul)  {
+		
+		String answer = "";
+		
+		for(int i=0; i<seoul.length; i++) {
+			if(seoul[i].equals("Kim")) {
+				return "김서방은 " + i +"에 있다";
+			}
+		}
+
+		
+		return answer;
+		}
+
+
+	public static void main(String[] args) throws Exception{
+		String[] seoul = {"Jane", "Kim"};
+		
+		System.out.println("결과 : "+ solution(seoul));
+		
+	}
+}
