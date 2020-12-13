9cfbe54fd44b6e2e4663177f6ddc3126c64149a1
nullsector12 <rlamd0412@gmail.com>
2020-12-13 18:26:27 +0900

    20201208 Daily Algorithm Day54

--- Algorithm-Ex/src/daily_Algorithm/Day54_20201208.java
+++ Algorithm-Ex/src/daily_Algorithm/Day54_20201208.java
@@ -1,36 +1,34 @@
 package daily_Algorithm;
 
-
-import java.util.*;
-
 /*
- *  2020. 12. 01 Daily Algorithm 
- *  Day52 - Programmers Cording Test Level 1
- *  서울에서 김서방 찾기
+ *  2020. 12. 08 Daily Algorithm 
+ *  Day54 - Programmers Cording Test Level 1
+ *  수박수박수박수박수?
  * 
  */
 
 public class Day54_20201208 {
 	
-	public static String solution(String[] seoul)  {
+	public static String solution(int n) {
 		
 		String answer = "";
 		
-		for(int i=0; i<seoul.length; i++) {
-			if(seoul[i].equals("Kim")) {
-				return "김서방은 " + i +"에 있다";
+		for(int i=1; i<=n; i++) {
+			if(i%2 == 1) {
+				answer += "수";
+			}else if(i%2 == 0) {
+				answer += "박";
 			}
 		}
-
 		
 		return answer;
 		}
 
 
 	public static void main(String[] args) throws Exception{
-		String[] seoul = {"Jane", "Kim"};
+		int n = 10000;
 		
-		System.out.println("결과 : "+ solution(seoul));
+		System.out.println("결과 : "+ solution(n));
 		
 	}
 }
