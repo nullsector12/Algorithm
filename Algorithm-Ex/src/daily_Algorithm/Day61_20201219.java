71e9db6b05d8e1c731d85d505d21f0e76e22473b
SeungYeon Kim <rlamd0412@gmail.com>
2020-12-19 00:25:50 +0900

    20201219 Daily Algorithm Day61

---
+++ Algorithm-Ex/src/daily_Algorithm/Day61_20201219.java
@@ -0,0 +1,33 @@
+package daily_Algorithm;
+
+import java.util.*;
+
+/*
+ *  2020. 12. 19 Daily Algorithm 
+ *  Day61 - Programmers Cording Test Level 1
+ *  자연수 뒤집어 배열로 만들기
+ * 
+ */
+
+public class Day61_20201219 {
+	
+	public static ArrayList<Integer> solution(long n)  {
+		ArrayList<Integer> answer = new ArrayList<Integer>();
+		
+		while(n>0) {
+			answer.add((int) (n%10));
+			n = n/10;
+		}
+		
+		return answer;
+	}	
+	
+	
+	public static void main(String[] args) throws Exception{
+		
+		int n = 12345;
+		
+		System.out.println(solution(n));
+		
+	}
+}
